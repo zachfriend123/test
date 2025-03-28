c3bea8b16f9c75c3c953cd62d0f2c21b9ed3d960
zachfriend123 <thegamingzach@gmail.com>
2025-03-27 20:51:04 -0500

    Update lab4.js

--- lab4.js
+++ lab4.js
@@ -1,1 +1,24 @@
+const form = document.querySelector("form#register-form");
+const feed = document.querySelector("#feedback-alert");
 
+form?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const { first, last, email, password, birth } = e.target;
+  const userData = {
+    first: first.value,
+    last: last.value,
+    email: email.value,
+    password: password.value,
+    birth: birth.value,
+  };
+
+  // handle data
+  console.log(userData);
+
+  e.target.reset();
+
+  feed.classList.remove("d-none");
+  setTimeout(() => {
+    feed.classList.add("d-none");
+  }, 5000);
+});
